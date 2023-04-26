6d7862a76013e26b897531b0bca87dfebef636d3
SégolèneLazarus <seg.laz@ik.me>
2023-04-26 11:00:31 +0200

    en attendant florian

---
+++ build/CMakeFiles/OpenGLTemplate.dir/cmake_clean.cmake
@@ -0,0 +1,17 @@
+file(REMOVE_RECURSE
+  "C:/Users/HP/Documents/1ScolaireIMAC/OpenGL/testtemplateenguerrand/OpenGLTemplate-IMAC2025_glu/bin/OpenGLTemplate.exe"
+  "C:/Users/HP/Documents/1ScolaireIMAC/OpenGL/testtemplateenguerrand/OpenGLTemplate-IMAC2025_glu/bin/OpenGLTemplate.exe.manifest"
+  "C:/Users/HP/Documents/1ScolaireIMAC/OpenGL/testtemplateenguerrand/OpenGLTemplate-IMAC2025_glu/bin/OpenGLTemplate.pdb"
+  "CMakeFiles/OpenGLTemplate.dir/src/3D_tools.c.obj"
+  "CMakeFiles/OpenGLTemplate.dir/src/3D_tools.c.obj.d"
+  "CMakeFiles/OpenGLTemplate.dir/src/draw_scene.c.obj"
+  "CMakeFiles/OpenGLTemplate.dir/src/draw_scene.c.obj.d"
+  "CMakeFiles/OpenGLTemplate.dir/src/main.cpp.obj"
+  "CMakeFiles/OpenGLTemplate.dir/src/main.cpp.obj.d"
+  "libOpenGLTemplate.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/OpenGLTemplate.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
