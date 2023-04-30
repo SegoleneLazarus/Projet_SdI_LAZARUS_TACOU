b09759de10169d0260cc4cca40cbfb9f7e18d953
SégolèneLazarus <95088709+SegoleneLazarus@users.noreply.github.com>
2023-04-30 14:07:09 +0200

    Delete build directory

--- build/src/helpers/RootDir.hpp
+++
@@ -1,3 +0,0 @@
-#pragma once
-#define ROOT_DIR "C:/Users/HP/Documents/1ScolaireIMAC/OpenGL/testtemplateenguerrand/OpenGLTemplate-IMAC2025_glu/"
-//tips find here https://shot511.github.io/2018-05-29-how-to-setup-opengl-project-with-cmake/
